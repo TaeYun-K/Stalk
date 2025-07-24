36a5643298dd18be248d1dba07d6a4e598d41ae8
DESKTOP-DLOPF19\SSAFY <kty8600@naver.com>
2025-07-24 11:10:53 +0900

    init: 프로젝트 초기 세팅

---
+++ Stalk_BE/src/test/java/com/Stalk/hello/StalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Stalk.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
