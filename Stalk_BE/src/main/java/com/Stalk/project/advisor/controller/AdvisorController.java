3a9dc28813291c915ddc0e010ca6439b8e2cce3e
wonjun <dnjswns3851@naver.com>
2025-07-28 11:12:57 +0900

    [BE] feat: 전문가 조회 -  리뷰 수, 평점 기준 정렬

--- Stalk_BE/src/main/java/com/Stalk/project/advisor/controller/AdvisorController.java
+++ Stalk_BE/src/main/java/com/Stalk/project/advisor/controller/AdvisorController.java
@@ -3,6 +3,7 @@ package com.Stalk.project.advisor.controller;
 import com.Stalk.project.advisor.dto.in.AdvisorListRequestDto;
 import com.Stalk.project.advisor.dto.out.AdvisorResponseDto;
 import com.Stalk.project.advisor.service.AdvisorService;
+import com.Stalk.project.response.BaseResponse;
 import com.Stalk.project.util.CursorPage;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -16,8 +17,9 @@ public class AdvisorController {
     private final AdvisorService advisorService;
 
     @GetMapping
-    @Operation(summary = "어드바이저 목록 조회", description = "필터 조건에 따른 어드바이저 목록을 조회합니다.")
-    public CursorPage<AdvisorResponseDto> getAdvisorList(AdvisorListRequestDto requestDto) {
-        return advisorService.getAdvisorList(requestDto);
+    @Operation(summary = "어드바이저 목록 조회")
+    public BaseResponse<CursorPage<AdvisorResponseDto>> getAdvisorList(AdvisorListRequestDto requestDto) {
+        CursorPage<AdvisorResponseDto> result = advisorService.getAdvisorList(requestDto);
+        return new BaseResponse<>(result); // 생성자 사용
     }
 }
--- Stalk_BE/src/main/java/com/Stalk/project/advisor/dto/in/AdvisorListRequestDto.java
+++ Stalk_BE/src/main/java/com/Stalk/project/advisor/dto/in/AdvisorListRequestDto.java
@@ -13,9 +13,11 @@ import java.util.List;
 @ToString
 public class AdvisorListRequestDto extends PageRequestDto {
 
-    @Schema(description = "거래 스타일 필터 (SHORT, MID, LONG)",
-            example = "[\"SHORT\", \"LONG\"]") // 또는 "[\"LONG\"]"
+    @Schema(description = "거래 스타일 필터 (SHORT, MID, LONG)", example = "[\"SHORT\", \"LONG\"]")
     private List<PreferredTradeStyle> preferredTradeStyle;
+
+    @Schema(description = "정렬 기준 (REVIEW_COUNT: 리뷰수순, RATING: 평점순)", example = "REVIEW_COUNT")
+    private SortBy sortBy;
 
     @Schema(description = "커서 (다음 페이지 조회용)", example = "0")
     private Long cursor;
