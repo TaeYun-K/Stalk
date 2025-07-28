c63c54deda763a44758b9bc03c50df331d875100
DESKTOP-DLOPF19\SSAFY <kty8600@naver.com>
2025-07-28 16:31:07 +0900

    [BE] wip : work in progress

--- Stalk_BE/src/main/java/com/Stalk/project/openvidu/config/OpenViduConfig.java
+++ Stalk_BE/src/main/java/com/Stalk/project/openvidu/config/OpenViduConfig.java
@@ -1,5 +1,6 @@
 package com.Stalk.project.openvidu.config;
 
+import io.openvidu.java.client.OpenVidu;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
