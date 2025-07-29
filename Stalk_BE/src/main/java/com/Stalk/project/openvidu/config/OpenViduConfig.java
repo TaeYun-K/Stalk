b109e8e5f4b14d798ee513422d29d19e8419f296
DESKTOP-DLOPF19\SSAFY <kty8600@naver.com>
2025-07-29 17:23:59 +0900

    [INFRA] fix : nginx.conf openvidu SSL 터널링 HTTPs 터널링 HTTP로 변경

--- Stalk_BE/src/main/java/com/Stalk/project/openvidu/config/OpenViduConfig.java
+++ Stalk_BE/src/main/java/com/Stalk/project/openvidu/config/OpenViduConfig.java
@@ -8,14 +8,15 @@ import org.springframework.context.annotation.Configuration;
 // @Configuration 클래스에 등록
 @Configuration
 public class OpenViduConfig {
-    @Value("${openvidu.url}")
-    private String OPENVIDU_URL;
 
-    @Value("${openvidu.secret}")
-    private String OPENVIDU_SECRET;
+  @Value("${openvidu.url}")
+  private String OPENVIDU_URL;
 
-    @Bean
-    public OpenVidu openVidu() {
-        return new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
-    }
+  @Value("${openvidu.secret}")
+  private String OPENVIDU_SECRET;
+
+  @Bean
+  public OpenVidu openVidu() {
+    return new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
+  }
 }
