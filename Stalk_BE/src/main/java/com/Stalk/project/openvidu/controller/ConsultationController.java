949698a1e543df4e469e76070631bed0d300695a
DESKTOP-DLOPF19\SSAFY <kty8600@naver.com>
2025-07-29 14:24:06 +0900

    [INFRA] fix : openvidu 설정을 위해 ngin.conf 설정 추가

--- Stalk_BE/src/main/java/com/Stalk/project/openvidu/controller/ConsultationController.java
+++ Stalk_BE/src/main/java/com/Stalk/project/openvidu/controller/ConsultationController.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 import java.util.NoSuchElementException;
 
 @Tag(name = "상담 관련 Controller")
@@ -17,36 +16,37 @@ import java.util.NoSuchElementException;
 @RequestMapping("/api/consultations")
 public class ConsultationController {
 
-    private final ConsultationSessionService sessionService;
+  private final ConsultationSessionService sessionService;
 
-    public ConsultationController(ConsultationSessionService sessionService) {
-        this.sessionService = sessionService;
-    }
+  public ConsultationController(ConsultationSessionService sessionService) {
+    this.sessionService = sessionService;
+  }
 
 
-    @Operation(summary = "상담방 생성")
-    @PostMapping("/{reservationId}/session")
-    public ResponseEntity<SessionTokenResponseDto> createSessionToken(
-            @PathVariable String reservationId) {
-        try {
-            SessionTokenResponseDto dto =
-                    sessionService.createSessionAndGetToken(reservationId);
-            return ResponseEntity.status(HttpStatus.CREATED).body(dto);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+  @Operation(summary = "상담방 생성")
+  @PostMapping("/{consultationId}/session")
+  public ResponseEntity<SessionTokenResponseDto> createSessionToken(
+      @PathVariable String consultationId) {
+    try {
+      SessionTokenResponseDto dto =
+          sessionService.createSessionAndGetToken(consultationId);
+      return ResponseEntity.status(HttpStatus.CREATED).body(dto);
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
-    @Operation(summary = "상담방 생성 여부 조회")
-    @GetMapping("/{reservationId}/session")
-    public ResponseEntity<SessionInfoDto> getSession(
-            @PathVariable String reservationId) {
-        try {
-            SessionInfoDto info = sessionService.getSessionInfo(reservationId);
-            return ResponseEntity.ok(info);
-        } catch (NoSuchElementException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+  }
+
+  @Operation(summary = "상담방 생성 여부 조회")
+  @GetMapping("/{consultationId}/session")
+  public ResponseEntity<SessionInfoDto> getSession(
+      @PathVariable String consultationId) {
+    try {
+      SessionInfoDto info = sessionService.getSessionInfo(consultationId);
+      return ResponseEntity.ok(info);
+    } catch (NoSuchElementException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
+  }
 }
--- Stalk_BE/src/main/java/com/Stalk/project/openvidu/dto/out/SessionTokenResponseDto.java
+++ Stalk_BE/src/main/java/com/Stalk/project/openvidu/dto/out/SessionTokenResponseDto.java
@@ -6,10 +6,12 @@ import lombok.Data;
 import org.springframework.messaging.handler.annotation.SendTo;
 
 import java.time.Instant;
-@Schema(description = "상담방 생성시 보내는 response")
+
+@Schema(description = "상담방 생성 및 입장 시, 토큰을 발급하는 response")
 @Data
 @AllArgsConstructor
 public class SessionTokenResponseDto {
+
   private final String sessionId;
   private final String token;
   private final String createdAt;
