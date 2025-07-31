639135e45a46380ed08077e00fae82c6e3828fac
김태윤 <kty8600@naver.com>
2025-08-01 02:03:49 +0900

    [FE] fix : mypage 하드코딩 수정

--- Stalk_FE/src/pages/my-page.tsx
+++ Stalk_FE/src/pages/my-page.tsx
@@ -264,7 +264,7 @@ const MyPage = () => {
     try {
       const consultationId = consultationItem.id;
 
-      const sessionData = await ConsultationService.createSessionToken("consultationId");
+      const sessionData = await ConsultationService.createSessionToken(consultationId);
       navigate( // parameter 여러개 넘기기
         `/video-consultation/${sessionData.sessionId}`,
         {
