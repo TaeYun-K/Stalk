22744dd856750656cfa2ae1ed035458aa1d38385
DESKTOP-DLOPF19\SSAFY <happyms817@gmail.com>
2025-08-01 15:19:44 +0900

    [BE] fix: AuthService

--- Stalk_BE/src/main/java/com/Stalk/project/login/service/AuthService.java
+++ Stalk_BE/src/main/java/com/Stalk/project/login/service/AuthService.java
@@ -32,6 +32,7 @@ public class AuthService {
 
   public LoginResponse login(LoginRequest loginRequest) {
     User user = userLoginMapper.findByUserId(loginRequest.getUserId());
+    System.out.println("user id: " + user.getId());
     if (user == null || !passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
       throw new RuntimeException("Invalid user ID or password");
     }
