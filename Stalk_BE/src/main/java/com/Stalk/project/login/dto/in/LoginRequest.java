9d84d8f0fd2ca08a6fa92cb8a3af1fbf1b6f11c3
DESKTOP-DLOPF19\SSAFY <happyms817@gmail.com>
2025-08-01 13:51:03 +0900

    [BE] fix: login

--- Stalk_BE/src/main/java/com/Stalk/project/login/dto/in/LoginRequest.java
+++ Stalk_BE/src/main/java/com/Stalk/project/login/dto/in/LoginRequest.java
@@ -1,10 +1,8 @@
 package com.Stalk.project.login.dto.in;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class LoginRequest {
 
   private String userId;
