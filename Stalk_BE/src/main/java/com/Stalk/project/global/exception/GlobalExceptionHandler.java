d73163c6ea46249380e2a66da77c454e66871aff
wonjun <dnjswns3851@naver.com>
2025-08-05 12:14:34 +0900

    [BE] feat: 커뮤니티 글에 댓글 달리면 알림 전송

--- Stalk_BE/src/main/java/com/Stalk/project/global/exception/GlobalExceptionHandler.java
+++ Stalk_BE/src/main/java/com/Stalk/project/global/exception/GlobalExceptionHandler.java
@@ -6,10 +6,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Slf4j
-//@RestControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
   /**
--- Stalk_BE/src/main/java/com/Stalk/project/global/notification/event/CommentCreatedEvent.java
+++ Stalk_BE/src/main/java/com/Stalk/project/global/notification/event/CommentCreatedEvent.java
@@ -2,16 +2,33 @@ package com.Stalk.project.global.notification.event;
 
 import lombok.Getter;
 
+/**
+ * 커뮤니티 댓글 작성 시 발행되는 이벤트
+ */
 @Getter
 public class CommentCreatedEvent extends NotificationEvent {
-    private final Long commentId;
-    private final String commenterName;
-    private final String postTitle;
-    
-    public CommentCreatedEvent(Long postAuthorId, Long commentId, String commenterName, String postTitle) {
-        super(postAuthorId, String.format("%s님이 '%s' 글에 댓글을 남겼습니다.", commenterName, postTitle));
+
+    private final Long postId;              // 글 ID
+    private final Long commentId;           // 댓글 ID
+    private final Long commentAuthorId;     // 댓글 작성자 ID
+    private final String commentAuthorName; // 댓글 작성자 닉네임
+    private final String commentContent;    // 댓글 내용
+    private final Long postAuthorId;        // 글 작성자 ID (알람 수신자)
+    private final String postTitle;         // 글 제목
+
+    public CommentCreatedEvent(Long postId, Long commentId, Long commentAuthorId,
+        String commentAuthorName, String commentContent,
+        Long postAuthorId, String postTitle) {
+        // 부모 클래스 생성자 호출 (targetUserId, message)
+        super(postAuthorId, String.format("%s님이 '%s' 글에 댓글을 남겼습니다.",
+            commentAuthorName, postTitle));
+
+        this.postId = postId;
         this.commentId = commentId;
-        this.commenterName = commenterName;
+        this.commentAuthorId = commentAuthorId;
+        this.commentAuthorName = commentAuthorName;
+        this.commentContent = commentContent;
+        this.postAuthorId = postAuthorId;
         this.postTitle = postTitle;
     }
 }
